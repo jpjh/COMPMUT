3161209ac59896eec2d45c7b31a175698bf021bf
jpjhall <jamespjhall@gmail.com>
2021-02-06 15:45:30 +0000

    Trying imagine scaling again...

--- functions/theme_pub.R
+++ functions/theme_pub.R
@@ -3,12 +3,12 @@ theme_pub <- function(base_size = 12, base_family = "Helvetica")
     half_line <- base_size/4
     theme_grey(base_size = base_size, base_family = base_family) %+replace% 
     theme(
-        axis.title=element_text(size=6),
-        axis.text=element_text(size=6, colour="black"),
+        axis.title=element_text(size=rel(0.5)),
+        axis.text=element_text(size=rel(0.5), colour="black"),
         axis.line = element_line(colour = "black"),
         legend.position="none",
-        legend.text=element_text(size=6),
-        legend.title=element_text(size=7),
+        legend.text=element_text(size=rel(0.5)),
+        legend.title=element_text(size=base_size*0.6),
         legend.key = element_blank(),
         legend.key.size=unit(0.5, units="lines"),
         legend.background = element_blank(),
@@ -19,12 +19,12 @@ theme_pub <- function(base_size = 12, base_family = "Helvetica")
         panel.grid.minor = element_blank(),
         panel.spacing.y=unit(0.55, units="lines"),
         panel.spacing.x=unit(0.2, units="lines"),
-        plot.margin=unit(c(2,2,0.1,0.1), units="mm"), # modified 7/11/17 to correct errors
-        plot.title=element_text(size=10),
+        plot.margin=unit(c(2,2,0.1,0.1), units="mm"),
+        plot.title=element_text(size=rel(0.8)),
         rect=element_rect(fill="white", colour="black", size=0.35, linetype=1),
         strip.background = element_blank(),
-        strip.text.y=element_text(size=6, margin = margin(t = half_line, b = half_line)),
-        strip.text.x=element_text(size=6, margin = margin(t = half_line, b = half_line)),
+        strip.text.y=element_text(size=rel(0.6), margin = margin(t = half_line, b = half_line)),
+        strip.text.x=element_text(size=rel(0.6), margin = margin(t = half_line, b = half_line)),
         complete = TRUE      
     )
 }
